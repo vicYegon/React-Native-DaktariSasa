261519f43e7495576a41f19ae496a130cfd72b90
vicYegon <vkosgei37@gmail.com>
2023-07-10 13:31:44 +0300

    first commit

---
+++ screens/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Button, Text, ScrollView } from "react-native";
+import { globalStyles } from "../styles/global";
+
+export default function Home(){
+    return(
+        <View style={globalStyles.container}>
+            <Text style={globalStyles.titleText}></Text>
+        </View>
+    )
+}
---
+++ styles/global.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"; 
+
+export const globalStyles = StyleSheet.create({    
+    container:{
+        flex: 1,
+        // padding: 24,
+    },
+    titleText:{
+        fontSize: 24,
+        fontWeight: 'bold',
+    }
+})
